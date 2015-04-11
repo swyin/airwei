ad7b3446ba30a1e5c8edfc503e3aff308837f8ae
swyin <swyinoz@hotmail.com>
2015-04-11 10:22:20 +0800

    first commit

---
+++ js/school.js
@@ -0,0 +1,271 @@
+$(document).ready(function () {
+    $("#sichuan").click(function () {
+
+        $("#sichuans").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#guangdong").click(function () {
+
+        $("#guangdongs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#beijing").click(function () {
+
+        $("#beijings").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#shanghai").click(function () {
+
+        $("#shanghais").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#chongqing").click(function () {
+
+        $("#chongqings").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#tianjin").click(function () {
+
+        $("#tianjins").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#xianggang").click(function () {
+
+        $("#xianggangs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#aomen").click(function () {
+
+        $("#aomens").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#taiwan").click(function () {
+
+        $("#taiwans").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#hebei").click(function () {
+
+        $("#hebeis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#jilin").click(function () {
+
+        $("#jilins").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#neimenggu").click(function () {
+
+        $("#neimenggus").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#guangxi").click(function () {
+
+        $("#guangxis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#zhejiang").click(function () {
+
+        $("#zhejiangs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#anhui").click(function () {
+
+        $("#anhuis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#fujian").click(function () {
+
+        $("#fujians").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#shanxi").click(function () {
+
+        $("#shanxis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#liaoning").click(function () {
+
+        $("#liaonings").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#gansu").click(function () {
+
+        $("#gansus").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#jiangxi").click(function () {
+
+        $("#jiangxis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#xizang").click(function () {
+
+        $("#xizangs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#henan").click(function () {
+
+        $("#henans").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#hubei").click(function () {
+
+        $("#hubeis").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#hunan").click(function () {
+
+        $("#hunans").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#xinjiang").click(function () {
+
+        $("#xinjiangs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#shandong").click(function () {
+
+        $("#shandongs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#yunnan").click(function () {
+
+        $("#yunnans").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#heilongjiang").click(function () {
+
+        $("#heilongjiangs").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#jiangsu").click(function () {
+
+        $("#jiangsus").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#shanxi2").click(function () {
+
+        $("#shanxi2s").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#guizhou").click(function () {
+
+        $("#guizhous").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#qinghai").click(function () {
+
+        $("#qinghais").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#ningxia").click(function () {
+
+        $("#ningxias").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function () {
+    $("#hainan").click(function () {
+
+        $("#hainans").slideToggle("slow");
+
+    });
+});
